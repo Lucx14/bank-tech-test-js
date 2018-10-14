1b828eef49fdb1c6cb46b0e314bf5d8f99e0cc93
Lucien Najev <luciennajev@hotmail.com>
2018-10-14 13:16:58 +0100

    features: first 4 basic tests written and passed

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,12 +1,41 @@
 describe('Account', () => {
-    let account;
+  let account;
 
-    beforeEach(function() {
-        account = new Account();
-    });
+  beforeEach(function() {
+    account = new Account();
+  });
 
+
+
+  describe('#deposit', () => {
     it('a user can make a deposit', () => {
-        account.deposit(1000);
-        expect(account.balance).toEqual(1000);
+      account.deposit(1000);
+      expect(account.currentBalance()).toEqual(1000);
     });
+  });
+
+
+  describe('#withdraw', () => {
+    it('a user can make a withdrawal', () => {
+      account.deposit(1000);
+      account.withdraw(500);
+      expect(account.currentBalance()).toEqual(500);
+      
+    });
+  });
+
+
+  describe('#statement', () => {
+    it('a user can request a printed statement', () => {
+      expect(account.statement()).toEqual('statement');
+    });
+  });
+
+  describe('#currentBalance', () => {
+    it('a user can check the balance on the account', () => {
+      expect(account.currentBalance()).toEqual(0);
+    });
+  });
+
+
 });
--- src/Account.js
+++ src/Account.js
@@ -1,8 +1,21 @@
 function Account() {
-    this.balance = 0;
-    this.cashflows = [];
+  this._balance = 0;
+  this._cashflows = [];
 }
 
 Account.prototype.deposit = function(amount) {
-    this.balance += amount;
+  this._balance += amount;
+};
+
+
+Account.prototype.withdraw = function(amount) {
+  this._balance -= amount;
+};
+
+Account.prototype.statement = function() {
+  return 'statement';
+};
+
+Account.prototype.currentBalance = function() {
+  return this._balance;
 };
