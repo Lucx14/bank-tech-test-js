a75c42a96265f219219dba7e4910524a2ac79c94
Lucien Najev <luciennajev@hotmail.com>
2018-11-01 11:30:54 +0000

    feature: printer can now print a statement

--- spec/PrinterSpec.js
+++ spec/PrinterSpec.js
@@ -6,7 +6,7 @@ describe('Printer', () => {
   });
 
 
-  describe('#print', () => {
+  describe('#printStatement', () => {
     it('can print a formatted statement', () => {
       const date = new Date().toLocaleDateString();
       const transaction1 = {
@@ -19,24 +19,7 @@ describe('Printer', () => {
         _date: date, _credit: null, _debit: 500, _balance: 2500,
       };
       const cashflows = [transaction1, transaction2, transaction3];
-      expect(printer.printStatement(cashflows)).toEqual('date || credit || debit || balance');
-    });
-  });
-
-  describe('#headers', () => {
-    it('can print the headers of the statement', () => {
-      expect(printer.headers()).toEqual('date || credit || debit || balance');
-    });
-  });
-
-  describe('#body', () => {
-    it('can print the body of the statement', () => {
-      const date = new Date().toLocaleDateString();
-      const transaction1 = {
-        _date: date, _credit: 1000, _debit: null, _balance: 1000,
-      };
-      const cashflows = [transaction1];
-      expect(printer.body(cashflows)).toEqual('31/10/2018 || 1000 || null || 1000')
+      expect(printer.printStatement(cashflows)).toEqual('date || credit || debit || balance\n01/11/2018 || 1000 || null || 1000\n01/11/2018 || 2000 || null || 3000\n01/11/2018 || null || 500 || 2500');
     });
   });
 });
--- src/Printer.js
+++ src/Printer.js
@@ -1,18 +1,11 @@
 /* eslint no-underscore-dangle: 0 */
 function Printer() {}
 
+
 Printer.prototype.printStatement = function printStatement(cashflows) {
-  return 'date || credit || debit || balance';
-};
-
-Printer.prototype.headers = function headers() {
-  return 'date || credit || debit || balance';
-};
-
-Printer.prototype.body = function body(cashflows) {
-  let result = '';
+  let result = 'date || credit || debit || balance';
   cashflows.forEach((cashflow) => {
-    result = result.concat(`${cashflow._date} || ${cashflow._credit} || ${cashflow._debit} || ${cashflow._balance}`);
+    result = result.concat('\n', `${cashflow._date} || ${cashflow._credit} || ${cashflow._debit} || ${cashflow._balance}`);
   });
   return result;
 };
